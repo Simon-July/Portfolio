949cf278f54220c542a14b623c76366fb51c78b2
nanase <nanase@Nanases-MacBook-Air.local>
2022-05-22 00:00:07 +0900

    edit IntroductionBox

--- components/IntroductionBox.tsx
+++ components/IntroductionBox.tsx
@@ -20,7 +20,7 @@ export default function IntroductionBox () {
     <PicWrap src="images/hobby/bike.png" />
     <DivBox>
         <h2>趣味</h2>
-        <div style={{fontSize:"16px"}}>・本：米原万里・阿刀田高・中野京子<br/>・プラモデル制作<br/>・英語：最近はTOEFL ibt対策中<br />・愛車のCD50sでぶらぶら</div>
+        <div style={{fontSize:"15px"}}>・本：米原万里・阿刀田高・中野京子<br/>・プラモデル制作<br/>・英語：最近はTOEFL ibt対策中<br />・愛車のCD50sでぶらぶら</div>
     </DivBox>
     </Wrap>
     </>
@@ -28,6 +28,7 @@ export default function IntroductionBox () {
 };
 
 const Wrap = styled.div`
+    ${sp`
     background-color: #eee;
     padding: 1em 1em 1em 1em;
     display: flex;
@@ -40,14 +41,63 @@ const Wrap = styled.div`
     &:nth-child(even){
         flex-direction: row-reverse;
     }
+    `}
+    ${tab`
+    background-color: #eee;
+    padding: 1em 1em 1em 1em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    &:last-child{
+        margin-bottom: 80px;
+    }
+    &:nth-child(even){
+        flex-direction: row-reverse;
+    }
+    `}
+    ${pc`
+    background-color: #eee;
+    padding: 1em 1em 1em 1em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    &:last-child{
+        margin-bottom: 80px;
+    }
+    &:nth-child(even){
+        flex-direction: row-reverse;
+    }
+    `}
 `;
     
 const PicWrap = styled.img`
+    ${sp`
+    display: block;
+    width: 100%;
+    height: auto;
+    `}
+    ${tab`
     display: block;
     width: 48%;
     height: auto;
+    `}
+    ${pc`
+    display: block;
+    width: 48%;
+    height: auto;
+    `}
 `;
 
 const DivBox = styled.div`
+    ${sp`
+    width: 100%;
+    `}
+    ${tab`
     width: 48%;
+    `}
+    ${pc`
+    width: 48%;
+    `}
 `;
